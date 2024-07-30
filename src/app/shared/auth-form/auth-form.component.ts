4f460ccd09aaa7e60d3a153d0909b5c96f595e55
Tamta <tamtakopadz16@gmail.com>
2024-07-30 13:15:59 +0400

    Added Layout for Auth Pages

---
+++ src/app/shared/auth-form/auth-form.component.ts
@@ -0,0 +1,50 @@
+import { NgClass} from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth-form',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgClass],
+  templateUrl: './auth-form.component.html',
+  styleUrl: './auth-form.component.scss',
+})
+export class AuthFormComponent {
+  @Input() formGroup!: FormGroup;
+  @Input() title!: string;
+  @Input() warning!: string;
+  @Input() formFields!: { name: string; type: string; placeholder: string }[];
+  @Input() buttons!: { text: string; type: string; cssClass: string;imgSrc?: string }[];
+  @Input() redirectLabel!: string;
+  getErrorMessage(controlName: string): string {
+    const control = this.formGroup.get(controlName);
+
+    if (!control) {
+      return '';
+    }
+
+    if (
+      (control.hasError('required') || control.hasError('email')) &&
+      controlName === 'email'
+    ) {
+      return 'Please enter a valid email address.';
+    }
+
+    if (
+      (control.hasError('minlength') ||
+        control.hasError('maxlength') ||
+        control.hasError('required')) &&
+      controlName === 'password'
+    ) {
+      return 'The password should have a length between 6 and 30 characters.';
+    }
+
+    if (controlName === 'confirmPassword') {
+      if (control.hasError('notMatch')) {
+        return 'Passwords should match.';
+      }
+    }
+
+    return '';
+  }
+}
